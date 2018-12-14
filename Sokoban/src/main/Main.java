ea17dc9f6575c8abaefc7edfa165b76438b4fce9
Ali Mac <mralimac@users.noreply.github.com>
2018-12-14 15:47:35 +0000

    Add files via upload

--- Sokoban/src/main/Main.java
+++ Sokoban/src/main/Main.java
@@ -7,11 +7,11 @@ public class Main
 
 	public static void main(String[] args) 
 	{		
-		//GUI graphics = new GUI();		
-		//graphics.movePlayer();
-		//graphics.generateScreen();
-		Gameplay game1 = new Gameplay();
-		game1.countNumOfLevels("nah");
+		GUI graphics = new GUI();		
+		graphics.movePlayer();
+		graphics.generateScreen();
+		//Gameplay game1 = new Gameplay();
+		//game1.countNumOfLevels("nah");
 	}
 
 }
